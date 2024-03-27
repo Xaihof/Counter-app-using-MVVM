425925189fa1918cdbfff87a2585ec7c6b76d700
XAIHOF <abdullahxaihof@gmail.com>
2024-03-27 16:19:53 +0500

    This is the first commit by XAIHOF.

---
+++ app/src/main/java/com/example/viewmodellifecycleawarecomponents/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.viewmodellifecycleawarecomponents
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodellifecycleawarecomponents.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        setText()
+
+        binding.button.setOnClickListener {
+            mainViewModel.increment()
+            setText()
+        }
+
+    }
+
+    private fun setText() {
+        binding.textView.text = mainViewModel.count.toString()
+    }
+}
